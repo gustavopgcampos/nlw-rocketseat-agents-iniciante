8a7b0bfc0805be7006af338ec506ae7224043319
Gustavo Pereira Goncalves de Campos <gpereira@sg.unicamp.br>
2025-07-17 10:06:34 -0300

    feat: add js functions & finishing the second ep

---
+++ script.js
@@ -0,0 +1,11 @@
+const apiKeyInput = document.getElementById('apiKey')
+const apiResponse = document.getElementById('apiResponse')
+const gameSelectInput = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (event) => {
+    // irá deixar de fazer o padrão
+    event.preventDefault()
+})
